b1b54e7d1d37a064a98fa665e274d95beeda711f
Remi <not.iremy@gmail.com>
2020-04-10 19:17:18 +0200

    second commit

--- index.php
+++ index.php
@@ -3,11 +3,144 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet">
+   
     
     <title>Looking For Pals</title>
 </head>
 <body>
-    looking for pals from scratch project
+
+
+<!-- CALL HEADER -->
+<?php @include ('shared/header.php'); ?>
+
+
+<div class="container">
+            <div class="jumbotron mt-3">
+                <h1 class="display-3">Welcome on Looking for pals Project</h1>
+                <p class="lead">Looking for pals is top site for meet people from everywhere
+                    exchange letters penpals culture travelling plan and everything you want
+                </p>
+                <hr class="my-4">
+                <p>Looking for a pals on a specific country ?</p>
+                <p class="lead">
+                    <a class="btn btn-info btn-lg" style="padding: 1rem 2rem; border-radius:40px" href="#" role="button">Meet people</a>
+                </p>
+            </div>   
+               
+            <h2>Chat with people from all over the world !</h2>
+
+            <div class="row mt-5">
+                <div class="col-md-4">
+                    <div class="card bg-light mb-3">
+                        
+
+                        <img src="http://placehold.it/1000x400" alt="" style="height: 200px; width:100%; display:block;">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                Profil 1
+                            </h4>
+                            <p class="card-text">
+                                But I must explain to you how all this 
+                                mistaken idea of denouncing pleasure 
+                                and praising pain was born and I will give 
+                                you a complete account of the system, and
+                            </p>
+                            <a href="" class="btn btn-secondary">See more...</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card bg-light mb-3">
+                        
+
+                        <img src="http://placehold.it/1000x400" alt="" style="height: 200px; width:100%; display:block;">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                profil 2
+                            </h4>
+                            <p class="card-text">
+                                But I must explain to you how all this 
+                                mistaken idea of denouncing pleasure 
+                                and praising pain was born and I will give 
+                                you a complete account of the system, and
+                            </p>
+                            <a href="" class="btn btn-secondary">See more...</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card bg-light mb-3">
+                        
+
+                        <img src="http://placehold.it/1000x400" alt="" style="height: 200px; width:100%; display:block;">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                profil 3
+                            </h4>
+                            <p class="card-text">
+                                But I must explain to you how all this 
+                                mistaken idea of denouncing pleasure 
+                                and praising pain was born and I will give 
+                                you a complete account of the system, and
+                            </p>
+                            <a href="" class="btn btn-secondary">See more...</a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-5">
+                <div class="col">
+                    <div class="card border-secondary mb-3">
+                        <div class="card-body">
+                            <h4 class="card-title">Remi</h4>
+                            <hr>
+                            <div class="card-text">
+                                <img src="http://placehold.it/64x64" alt="avatar de remi" class="float-left mr-3">
+                                <p>
+                                    But I must explain to you how all this 
+                                mistaken idea of denouncing pleasure 
+                                and praising pain was born and I will give 
+                                you a complete account of the system
+                                </p>
+                                <a href="" class="btn btn-success float-right">See more...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card border-secondary mb-3">
+                        <div class="card-body">
+                            <h4 class="card-title">Remi</h4>
+                            <hr>
+                            <div class="card-text">
+                                <img src="http://placehold.it/64x64" alt="avatar de remi" class="float-left mr-3">
+                                <p>
+                                    But I must explain to you how all this 
+                                mistaken idea of denouncing pleasure 
+                                and praising pain was born and I will give 
+                                you a complete account of the system
+                                </p>
+                                <a href="" class="btn btn-success float-right">See more...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+</div>
+
+<!-- CALL FOOTER -->
+<?php @include ('shared/footer.php'); ?>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
